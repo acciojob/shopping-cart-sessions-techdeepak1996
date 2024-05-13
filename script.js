321ebfd143aa76b3a098528781ef38d1f9d8e73c
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-14 03:25:43 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 // Product data
-() => {
-  cy.get("ul#product-list").children("li").first().children("button").click();
-  cy.window().its("sessionStorage").should("have.length", 1);
-  cy.window().its("sessionStorage").invoke("getItem", "cart").should("eq", JSON.stringify([{ id: 1, name: "Product 1", price: 10, quantity: 2 }, { id: 5, name: "Product 5", price: 50, quantity: 1 }]));
-}
+const products = [
+  { id: 1, name: "Product 1", price: 10 },
+  { id: 2, name: "Product 2", price: 20 },
+  { id: 3, name: "Product 3", price: 30 },
+  { id: 4, name: "Product 4", price: 40 },
+  { id: 5, name: "Product 5", price: 50 },
+];
 
 // DOM elements
 const productList = document.getElementById("product-list");
@@ -46,8 +48,6 @@ function addToCart(productId) {
     renderCart();
   }
 }
-
-
 
 // Clear cart
 function clearCart() {
