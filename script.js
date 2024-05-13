0ee4e26ea8d931531c3dae0ae0892bca48b66a46
Tech Deepak <141490582+techdeepak1996@users.noreply.github.com>
2024-05-14 02:10:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -47,16 +47,15 @@ function renderCart() {
   }
 }
 
+// Add item to cart
 function addToCart(productId) {
   const product = products.find((item) => item.id === productId);
   if (product) {
-    let cart = JSON.parse(sessionStorage.getItem("cart")) || []; // Initialize cart if null
+    let cart = JSON.parse(sessionStorage.getItem("cart")) || [];
     const existingProductIndex = cart.findIndex((item) => item.id === productId);
     if (existingProductIndex !== -1) {
-      // If product already exists in cart, increase its quantity
       cart[existingProductIndex].quantity = (cart[existingProductIndex].quantity || 1) + 1;
     } else {
-      // If product is not in cart, add it with quantity 1
       cart.push({ ...product, quantity: 1 });
     }
     sessionStorage.setItem("cart", JSON.stringify(cart));
